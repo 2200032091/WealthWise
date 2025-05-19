0111a4546104fe0108fa650f86040f3d363a3492
2200032091 <gharika109@gmail.com>
2025-05-19 21:45:27 +0530

    stock dashboard basic outlook with crud is done

--- stock/stock-client/src/App.jsx
+++ stock/stock-client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import Watchlist from './pages/Watchlist';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/watchlist" element={<Watchlist />} />
       </Routes>
     </Router>
   );
