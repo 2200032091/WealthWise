3c3aa7e8640cd4b63a80813cf197cff99706e418
2200032091 <gharika109@gmail.com>
2025-10-01 15:10:02 +0530

    api changing

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://ec2-34-229-215-209.compute-1.amazonaws.com:5000/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
